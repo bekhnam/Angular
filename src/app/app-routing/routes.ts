f437273c68c40ce67d09654066ddfc05a624150a
Ngo Thanh Nam <ngothanhnam2016@gmail.com>
2021-07-13 17:23:19 +0300

    Single Page Applications Part 2

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -11,6 +11,6 @@ export const routes: Routes = [
     {path: 'menu', component: MenuComponent},
     {path: 'aboutus', component: AboutComponent},
     {path: 'contactus', component: ContactComponent},
-    {path: 'dishdetail', component: DishdetailComponent},
+    {path: 'dishdetail/:id', component: DishdetailComponent},
     {path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
