e6769af40063fec082e10967938d2f514cf5039c
Ngo Thanh Nam <ngothanhnam2016@gmail.com>
2021-07-14 09:00:37 +0300

    Assignment 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,10 +19,11 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 
+import { AppRoutingModule } from './app-routing/app-routing.module';
+
+import { LeaderService } from './services/leader.service';
 import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
-
-import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -48,7 +49,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
   ],
   providers: [
     DishService,
-    PromotionService
+    PromotionService,
+    LeaderService
   ],
   bootstrap: [AppComponent]
 })
